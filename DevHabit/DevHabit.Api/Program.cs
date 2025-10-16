5836480bda28bd2cfea17dcb6b4acbd35531e14c
Marco Silva <marco.p.c.silva@gmail.com>
2025-10-16 20:52:09 +0200

    Introducing the Database, EF Code

--- DevHabit/DevHabit.Api/Program.cs
+++ DevHabit/DevHabit.Api/Program.cs
@@ -1,3 +1,8 @@
+using DevHabit.Api.Database;
+using DevHabit.Api.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -9,12 +14,21 @@ builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options
+        .UseNpgsql(
+            builder.Configuration.GetConnectionString("Database"), 
+            npgsqlOptions => npgsqlOptions
+                .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+    .UseSnakeCaseNamingConvention());
+
 // Set services required for OpenTelemetry
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
     .WithTracing(tracing => tracing
         .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation())
+        .AddAspNetCoreInstrumentation()
+        .AddNpgsql())
     .WithMetrics(metrics => metrics
         .AddHttpClientInstrumentation()
         .AddAspNetCoreInstrumentation()
@@ -22,7 +36,7 @@ builder.Services.AddOpenTelemetry()
     .UseOtlpExporter();
 builder.Logging.AddOpenTelemetry(options =>
 {
-    options.IncludeScopes = true;
+    options.IncludeScopes = true; 
     options.IncludeFormattedMessage = true;
 });
 
@@ -31,6 +45,8 @@ WebApplication app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    await app.ApplyMigrationAsync();
 }
 
 app.UseHttpsRedirection();
