50abd64df01e5d69194ea0e00e8a9e75f573e84e
Marco Silva <marco.p.c.silva@gmail.com>
2025-10-25 16:39:33 +0200

    REST API Validation, Problem Details

--- DevHabit/DevHabit.Api/Controllers/HabitsController.cs
+++ DevHabit/DevHabit.Api/Controllers/HabitsController.cs
@@ -1,6 +1,8 @@
 ﻿using DevHabit.Api.Database;
 using DevHabit.Api.DTOs.Habits;
 using DevHabit.Api.Entities;
+using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
@@ -45,8 +47,16 @@ public sealed class HabitsController(ApplicationDbContext dbContext) : Controlle
     }
 
     [HttpPost]
-    public async Task<ActionResult<HabitDto>> CreateHabit(CreateHabitDto createHabitDto)
+    public async Task<ActionResult<HabitDto>> CreateHabit(
+        CreateHabitDto createHabitDto,
+        IValidator<CreateHabitDto> validator)
     {
+        ValidationResult validationResult = await validator.ValidateAsync(createHabitDto);
+        if(!validationResult.IsValid)
+        {
+            return BadRequest(validationResult.ToDictionary());
+        }
+
         Habit habit = createHabitDto.ToEntity();
 
         dbContext.Habits.Add(habit);
--- DevHabit/DevHabit.Api/DTOs/Tags/CreateTagDto.cs
+++ DevHabit/DevHabit.Api/DTOs/Tags/CreateTagDto.cs
@@ -1,7 +1,11 @@
-﻿namespace DevHabit.Api.DTOs.Tags;
+﻿//using System.ComponentModel.DataAnnotations;
+namespace DevHabit.Api.DTOs.Tags;
 
 public sealed record CreateTagDto
 {
+    //[Required]
+    //[MinLength(3)]
     public required string Name { get; set; }
+    //[MaxLength(50)]
     public string? Description { get; set; }
 }
