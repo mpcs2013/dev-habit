cfd7d001742cf9465f78dea556966f77f02b7673
Marco Silva <marco.p.c.silva@gmail.com>
2025-11-02 19:27:26 +0100

    Introducing ASP.NET Core Identity

--- DevHabit/DevHabit.Api/DependencyInjection.cs
+++ DevHabit/DevHabit.Api/DependencyInjection.cs
@@ -6,6 +6,7 @@ using DevHabit.Api.Middleware;
 using DevHabit.Api.Services;
 using DevHabit.Api.Services.Sorting;
 using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
@@ -90,6 +91,14 @@ public static class DependencyInjection
                         .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
             .UseSnakeCaseNamingConvention());
 
+        builder.Services.AddDbContext<ApplicationIdentityDbContext>(options =>
+            options
+                .UseNpgsql(
+                    builder.Configuration.GetConnectionString("Database"),
+                    npgsqlOptions => npgsqlOptions
+                        .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Identity))
+            .UseSnakeCaseNamingConvention());
+
         return builder;
     }
 
@@ -133,4 +142,11 @@ public static class DependencyInjection
         return builder;
     }
 
+    public static WebApplicationBuilder AddAuthenticationServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationIdentityDbContext>();
+
+        return builder;
+    }
 }
--- DevHabit/DevHabit.Api/Extensions/DatabaseExtensions.cs
+++ DevHabit/DevHabit.Api/Extensions/DatabaseExtensions.cs
@@ -8,13 +8,19 @@ public static class DatabaseExtensions
     public static async Task ApplyMigrationAsync(this WebApplication app)
     {
         using IServiceScope scope = app.Services.CreateScope();
-        await using ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        await using ApplicationDbContext applicationDbContext = 
+            scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        
+        await using ApplicationIdentityDbContext identityDbContext =
+            scope.ServiceProvider.GetRequiredService<ApplicationIdentityDbContext>();
 
         try
         { 
-            await dbContext.Database.MigrateAsync();
-
-            app.Logger.LogInformation("Database migration applied successfully.");
+            await applicationDbContext.Database.MigrateAsync();
+            app.Logger.LogInformation("Application database migration applied successfully.");
+            
+            await identityDbContext.Database.MigrateAsync();
+            app.Logger.LogInformation("Identity database migration applied successfully.");
         }
         catch (Exception e)
         {
