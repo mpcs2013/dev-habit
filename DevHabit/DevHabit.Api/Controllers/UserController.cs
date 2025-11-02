9ce3f226f2c47fbb492f8471e529cbc2b5bd72f1
Marco Silva <marco.p.c.silva@gmail.com>
2025-11-02 18:23:07 +0100

    Adding the Users Resource

---
+++ DevHabit/DevHabit.Api/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using DevHabit.Api.Database;
+using DevHabit.Api.DTOs.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Controllers;
+
+[ApiController]
+[Route("users")]
+public class UserController(ApplicationDbContext dbContext) : ControllerBase
+{
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById(string id)
+    {
+        UserDto? user = await dbContext.Users
+            .Where(u => u.Id == id)
+            .Select(UserQueries.ProjectToDto())
+            .FirstOrDefaultAsync();
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+}
