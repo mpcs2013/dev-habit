46a44ec9b4c8c0e95fb3761aa991c9bf9e969382
Marco Silva <marco.p.c.silva@gmail.com>
2025-11-12 20:16:16 +0100

    Supporting Refresh Tokens

--- DevHabit/DevHabit.Api/Services/TokenProvider.cs
+++ DevHabit/DevHabit.Api/Services/TokenProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using DevHabit.Api.DTOs.Auth;
 using DevHabit.Api.Settings;
@@ -43,9 +44,10 @@ public sealed class TokenProvider(IOptions<JwtAuthOptions> options)
         return accessToken;
     }
 
-    private string GenerateRefreshToken()
+    private static string GenerateRefreshToken()
     {
-        // Implementation for generating refresh token goes here
-        return string.Empty;
+        byte[] randomBytes = RandomNumberGenerator.GetBytes(32);
+
+        return Convert.ToBase64String(randomBytes);
     }
 }
